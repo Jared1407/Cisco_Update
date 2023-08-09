f38ae1df70dbb2ece7aec5fb2623ff9af42578ee
Jared1407 <98782318+Jared1407@users.noreply.github.com>
2023-08-09 15:55:13 +0000

    8-9-23 8:55am

--- python-app/web_interface.py
+++ python-app/web_interface.py
@@ -5,16 +5,18 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def manage_devices():
-    #Only move on when the "Submit" button is pressed:
     if request.method == 'POST':
-        device_list = request.form['device_list']
-        tftp_ip = request.form.get('tftp_ip')  # Use get method to handle missing field
-        cfg_file = request.form.get('cfg_file')  # Use get method to handle missing field
-        selection = request.form['selection']
+        try:
+            device_list = request.form['device_list']
+            tftp_ip = request.form.get('tftp_ip')  # Use get method to handle missing field
+            cfg_file = request.form.get('cfg_file')  # Use get method to handle missing field
+            selection = request.form['selection']
 
-        MkV.main(device_list, tftp_ip, cfg_file, selection)
+            MkV.main(device_list, tftp_ip, cfg_file, selection)
 
-        return 'Devices managed successfully'
+            return 'Devices managed successfully'
+        except Exception as e:
+            return render_template('index.html', error=e)
     else:
         return render_template('index.html')
     
